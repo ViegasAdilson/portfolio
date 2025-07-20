845bba6c488ebfe45d55e34e054842a59fea03d0
Viegas Adilson <adilsonviegas@hotmail.com>
2025-07-20 08:54:45 +0100

    add git page config

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,18 @@ import Skills from "./pages/Skill/Skills";
 import MyCV from "./pages/MyCV/MyCV";
 import About from "./pages/About/About";
 import Project from "./pages/Projecto/Project";
-import PageNotFound from "./pages/PageNotFound/PageNotFound";
+// import PageNotFound from "./pages/PageNotFound/PageNotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />}>
-          <Route index element={<Project />} />
+          <Route path="portfolio" element={<Project />} />
           <Route path="skills" element={<Skills />} />
           <Route path="mycv" element={<MyCV />} />
           <Route path="about" element={<About />} />
-          <Route path="*" element={<PageNotFound />} />
+          {/* <Route path="*" element={<PageNotFound />} /> */}
         </Route>
       </Routes>
     </BrowserRouter>
